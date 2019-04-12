67a329916bfad914131f1dbe0fb7830fd24c5cfc
Channa Wijesinghe <manzyron@hotmail.com>
2019-04-12 17:44:01 -0400

    Add project files.

---
+++ DAL.FineDesignCupcakes/FDCDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DAL.FineDesignCupcakes.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.FineDesignCupcakes
+{
+    public class FDCDatabaseContext : DbContext
+        {
+            public FDCDatabaseContext(DbContextOptions<FDCDatabaseContext> options)
+                : base(options)
+            { }
+
+            public DbSet<Flavors> Flavors { get; set; }
+
+        }
+    
+}
---
+++ DAL.FineDesignCupcakes/Models/Flavors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DAL.FineDesignCupcakes.Models
+{
+
+    public class Flavors
+    {
+        public string FlavorName { get; set; }
+        public string Image { get; set; }
+
+        public string Thumbnail { get; set; }
+
+    }
+}
